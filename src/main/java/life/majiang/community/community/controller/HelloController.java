34c41fe6c3194f04c92fc60714576c5c06eac066
clown14 <785613198@qq.com>
2019-06-22 12:26:24 +0800

    init repo

---
+++ src/main/java/life/majiang/community/community/controller/HelloController.java
@@ -0,0 +1,24 @@
+package life.majiang.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @program: community
+ * @author: onion
+ * @create: 2019-06-21 16:25
+ **/
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
+
+    
