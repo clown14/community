d84d354deb5955d4aca8cf684671714b4f79128e
clown14 <785613198@qq.com>
2019-06-24 12:55:36 +0800

    extract github config from code to properties

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 ## Spring boot学习
 
 ## 资料
+[Spring 文档](https://spring.io/guides)  
+[Spirng web 文档](https://spring.io/guides/gs/serving-web-content/)  
+[es社区](https://elasticsearch.cn)  
+[Bootstrap文档](https://v3.bootcss.com/getting-started/)  
+[Github OAuth 文档](https://developer.github.com/apps/building-github-apps/creating-a-github-app/)
 
-https://spring.io/guides
-
-https://spring.io/guides/gs/serving-web-content/
+## 工具
---
+++ src/main/java/life/majiang/community/community/dto/GithubUser.java
@@ -0,0 +1,39 @@
+package life.majiang.community.community.dto;
+
+/**
+ * @program: community
+ * @author: onion
+ * @create: 2019-06-23 13:47
+ **/
+
+public class GithubUser {
+    private String name;
+    private Long id;
+    private String bio;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
+
+    
