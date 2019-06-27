5c0cc13221f8bf6c3174b3a97f38179111c4c109
clown14 <785613198@qq.com>
2019-06-27 14:09:01 +0800

    add check cookies

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -1,9 +1,13 @@
 package life.majiang.community.community.controller;
 
+import life.majiang.community.community.mapper.UserMapper;
+import life.majiang.community.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @program: community
@@ -14,14 +18,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class IndexController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model) {
-        model.addAttribute("name", name);
-        return "hello";
-    }
+    @Autowired
+    private UserMapper userMapper;
+
 
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+
         return "index";
     }
 }
