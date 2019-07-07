2ee1fbf30aa4c17abcbb36cdcba7f6ed26bca70c
clown14 <785613198@qq.com>
2019-07-07 20:23:17 +0800

    update question

--- src/main/java/life/majiang/community/community/mapper/QuestionMapper.java
+++ src/main/java/life/majiang/community/community/mapper/QuestionMapper.java
@@ -1,10 +1,7 @@
 package life.majiang.community.community.mapper;
 
 import life.majiang.community.community.model.Question;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -34,6 +31,11 @@ public interface QuestionMapper {
 
     @Select("select * from question where id=#{id}")
     Question getById(Integer id);
+
+    @Update("update question set title=#{title}, description=#{description}, gmt_modified=#{gmtModified}, tag=#{tag} where id=#{id}")
+//    @Update("update question set title = #{title}, description = #{description}, gmt_modified = #{gmtModified}, tag = #{tag} where id = #{id}")
+    void update(Question question);
+
 }
 
     
