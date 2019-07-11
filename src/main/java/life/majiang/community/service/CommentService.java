99532eafd6cc7195ba6dc98be17c636aaab6dd76
clown14 <785613198@qq.com>
2019-07-11 15:46:58 +0800

    事务

--- src/main/java/life/majiang/community/service/CommentService.java
+++ src/main/java/life/majiang/community/service/CommentService.java
@@ -10,6 +10,7 @@ import life.majiang.community.model.Comment;
 import life.majiang.community.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @program: community
@@ -29,6 +30,7 @@ public class CommentService {
     @Autowired
     private QuestionExtMapper questionExtMapper;
 
+    @Transactional
     public void insert(Comment comment) {
         if (comment.getParentId() == null || comment.getParentId() == 0) {
             throw new CustomizeException(CustomizeErrorCode.TARGET_PARAM_NOT_FOUND);
