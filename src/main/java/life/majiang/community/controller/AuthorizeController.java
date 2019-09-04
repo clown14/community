7c687a800a9e0bc20e6f58126effb1ff12889d2f
clown14 <785613198@qq.com>
2019-09-04 19:13:54 +0800

    追加日志

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -5,6 +5,7 @@ import life.majiang.community.dto.GithubUser;
 import life.majiang.community.model.User;
 import life.majiang.community.provider.GithubProvider;
 import life.majiang.community.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -23,6 +24,7 @@ import java.util.UUID;
  **/
 
 @Controller
+@Slf4j
 public class AuthorizeController {
 
     @Autowired
@@ -65,6 +67,7 @@ public class AuthorizeController {
             response.addCookie(new Cookie("token", token));
             return "redirect:/";
         } else {
+            log.error("callback get github error,{}", githubUser);
             //登陆失败，重新登陆
             return "redirect:/";
         }
